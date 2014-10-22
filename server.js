646f56c73d6360e06f76d4c32ed00aae55f935aa
Matt Clyde <mclyde@nextcenturymeters.com>
2014-10-22 13:34:36 -0600

    added public dir

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app.get('/', function(req, res){
-    res.send('hello world');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000);
+console.log('Server listening on port 3000');
